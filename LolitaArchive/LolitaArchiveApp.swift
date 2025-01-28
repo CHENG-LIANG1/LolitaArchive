abbdfd74f4c5f464cf86daa72d1f32bc83304e2d
Cheng <madfoxfoxy@gmail.com>
2025-01-28 14:03:42 +0800

    Initial Commit

---
+++ LolitaArchive/LolitaArchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  LolitaArchiveApp.swift
+//  LolitaArchive
+//
+//  Created by 梁非凡 on 2025/1/28.
+//
+
+import SwiftUI
+
+@main
+struct LolitaArchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LolitaArchiveTests/LolitaArchiveTests.swift
@@ -0,0 +1,17 @@
+//
+//  LolitaArchiveTests.swift
+//  LolitaArchiveTests
+//
+//  Created by 梁非凡 on 2025/1/28.
+//
+
+import Testing
+@testable import LolitaArchive
+
+struct LolitaArchiveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
